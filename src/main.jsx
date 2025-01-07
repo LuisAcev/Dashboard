f39fcc9056604492c95c4571c2d76d32a3eeb4d3
FELIPE <tecnomin3@gmail.com>
2025-01-06 21:00:13 -0500

    deploy #2

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
 import Desktop from './components/desktop/Desktop.jsx'
 
 createRoot(document.getElementById('root')).render(
